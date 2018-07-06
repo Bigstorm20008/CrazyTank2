eae6141bec88172b31edc0d3699e9c3986be2372
Bigstorm <bigstorm@3g.ua>
2018-07-06 14:22:54 +0300

    MenuState

--- MenuState.cpp
+++ MenuState.cpp
@@ -21,8 +21,13 @@ void MenuState::render()
 	if (!m_isVisible)
 	{
 		system("cls");
-		std::cout << "Game in menu" << std::endl;
-		std::cout << "Press S key for begin" << std::endl;
+		std::wcout << "    Game in menu" << std::endl;
+		std::wcout << "Press S key for begin" << std::endl;
+		std::wcout << "Press Q key for exit" << std::endl;
+		std::wcout << std::endl;
+		std::wcout << "     Tank controls:" << std::endl;
+		std::wcout << "Use arrows key for moving" << std::endl;
+		std::wcout << "Use space for fire" << std::endl;
 		m_isVisible = true;
 	}
 }
